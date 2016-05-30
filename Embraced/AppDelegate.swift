906ea93b404ce2479139ad67e1302be43b9dd517
domoniquedixon <domonique@lapetussolutions.com>
2016-05-30 11:02:37 -0400

    Added user login functionality

--- Embraced/AppDelegate.swift
+++ Embraced/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Stormpath
 import CoreData
 
 @UIApplicationMain
@@ -16,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let APIURL = "http://localhost:3000"
+        Stormpath.sharedSession.configuration.APIURL = NSURL(string: APIURL)!
         return true
     }
 
--- Embraced/ViewController.swift
+++ Embraced/ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Stormpath
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var nodeResponseView: UIWebView!
+    @IBOutlet weak var nameLabel: UILabel!
     
     let url = "http://girlscouts.harryatwal.com"
-    let nodeUrl = "http://54.201.210.104/test"
+    var nodeUrl = "http://localhost:3000"
     
     
     override func viewDidLoad() {
@@ -34,5 +38,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func login(sender: AnyObject) {
+        Stormpath.sharedSession.login(usernameTextfield.text!, password: passwordTextfield.text!, completionHandler: logResponse)
+    }
+    
+    func logResponse(success: Bool, error: NSError?) {
+        if let error = error {
+            showAlert(withTitle: "Error", message: error.localizedDescription)
+        }
+        else {
+            showAlert(withTitle: "Success", message: "You logged in successfully")
+            
+            Stormpath.sharedSession.me { (account, error) -> Void in
+                if let account = account {
+                    self.nameLabel.text = "Hello \(account.fullName)!"
+                }
+            }
+            
+            let notesEndpoint = NSURL(string: "http://localhost:3000/secret")!
+            let request = NSMutableURLRequest(URL: notesEndpoint)
+            request.setValue("Bearer \(Stormpath.sharedSession.accessToken!)", forHTTPHeaderField: "Authorization")
+//            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
+//                guard let data = data, json = try? NSJSONSerialization.JSONObjectWithData(data, options: []), notes = json["notes"] as? String else {
+//                    return
+//                }
+//                dispatch_async(dispatch_get_main_queue(), {
+////                    self.notesTextView.text = notes
+//                    
+//                })
+//            }
+//            task.resume()
+            
+            nodeResponseView.loadRequest(request)
+        }
+    }
 
+}
+
+// Helper extension to display alerts easily.
+extension UIViewController {
+    func showAlert(withTitle title: String, message: String?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
