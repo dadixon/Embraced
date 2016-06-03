77565fc860caf0aa19f3712224895ce2383230ce
domoniquedixon <domonique@lapetussolutions.com>
2016-06-03 19:46:07 -0400

    add workspace

---
+++ Pods/Target Support Files/Pods-Embraced/Pods-Embraced-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Embraced : NSObject
+@end
+@implementation PodsDummy_Pods_Embraced
+@end
---
+++ Pods/Target Support Files/Pods-Embraced/Pods-Embraced-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_EmbracedVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_EmbracedVersionString[];
+
